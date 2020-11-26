29af37722fd5d35d073e2810d40caaebffb4da48
Matt Dudzik <mdudzik92@gmail.com>
2020-11-25 22:04:56 -0600

    Set up routes in server.js

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/mattsworkouttracker',
     {
@@ -7,3 +22,10 @@ mongoose.connect(
       useFindAndModify: false
     }
   );
+
+  app.use(require("./routes/api.js"));
+  app.use(require("./routes/view.js"));
+
+  app.listen(PORT, () => {
+      console.log("Running on port " + PORT);
+  });
